fe0b48d005bc2422d588c91b3f06368501422b1d
flasophi <46344378+flasophi@users.noreply.github.com>
2026-05-11 08:49:19 +0000

    Deploying to gh-pages from @ flasophi/flasophi.github.io@7704aea6d808726aace6992028704d40b9fad61d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,13 +469,6 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0002-7880-2447", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
